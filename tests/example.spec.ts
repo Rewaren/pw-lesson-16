25553f68b403bbd00e2cde16551c6ae1804d1191
warenr02 <warenr02@gmail.com>
2026-01-11 20:00:52 +0200

    tests for TL sign in page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Tallinn/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has authorization error', async ({ page }) => {
+  await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByTestId('username-input').fill('maria');
+  await page.getByTestId('password-input').fill('password123');
+  await page.getByTestId('signIn-button').click();
+  await expect(page.getByTestId('authorizationError-popup-close-button')).toBeVisible();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('sign in disabled when valid username and invalid password', async ({ page }) => {
+    await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
+
+    await page.getByTestId('username-input').fill('maria');
+    await page.getByTestId('password-input').fill('psw12');
+    await expect(page.getByTestId('signIn-button')).toBeDisabled();
 });
